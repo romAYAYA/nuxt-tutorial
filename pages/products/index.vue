893764338562a0684b6b674f904e9d8cdaf04149
Roman Khabayev <supernaturalmlady@gmail.com>
2023-10-29 17:45:58 +0600

    product components added

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products" :key="p.id">
-        <nuxt-link :to="`/products/${p.id}`">{{ p.title }}</nuxt-link>
+        <ProductCard :product="p" />
       </div>
     </div>
   </div>
