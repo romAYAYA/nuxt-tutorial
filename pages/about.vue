10274d032995203e105684f20ef3aacc617b8512
Roman Khabayev <supernaturalmlady@gmail.com>
2023-10-29 16:22:07 +0600

    project created, some magic seen

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio fugiat
+      beatae necessitatibus omnis quod cumque earum magnam obcaecati quibusdam
+      possimus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam quidem
+      corrupti quaerat cum perspiciatis ea voluptas distinctio aut reprehenderit
+      dolorem.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
