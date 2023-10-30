fc712f3ba77a9616216ea55633644e1d53f7c8d6
Roman Khabayev <supernaturalmlady@gmail.com>
2023-10-30 17:41:29 +0600

    meta tags added, add to cart button added

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -14,6 +14,11 @@ definePageMeta({
 })
 
 const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+useHead({
+  title: 'Nuxt Dojo | Merch',
+  meta: [{ name: 'description', content: 'Nuxt 3 Merch' }],
+})
 </script>
 
 <style lang="scss" scoped></style>
