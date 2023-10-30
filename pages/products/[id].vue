865ff6574b2174090d83998cc70774ef8df3882a
Roman Khabayev <supernaturalmlady@gmail.com>
2023-10-30 17:13:41 +0600

    error handling

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,14 @@ const uri = `https://fakestoreapi.com/products/${id}`
 
 const { data: product } = await useFetch(uri, { key: id })
 
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found',
+    fatal: true,
+  })
+}
+
 definePageMeta({
   layout: 'products',
 })
